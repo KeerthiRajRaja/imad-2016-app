f9facf429eeed639892bb184936ed14eb91034c0
KeerthiRajRaja <keerthichathri@gmail.com>
2016-09-28 04:23:38 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element=document.getElementById('main-text');
 element.innerHTML='New value';
 
 var img = document.getElementById('madi');
+var marginleft=0;
+function moveright(){
+    marginleft=marginleft+10;
+    img.style.marginleft+'px';
+}
 img.onclick = function(){
-    img.style.marginleft = '100px';
+    var interval=setinterval(moveright,100);
 };
