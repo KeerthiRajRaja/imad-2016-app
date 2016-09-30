f8a9447b4442cd349e6f311505d77edc3e9f734c
KeerthiRajRaja <keerthichathri@gmail.com>
2016-09-30 16:33:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,8 +28,7 @@ button.onclick=function(){
   
 };
 //submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
+
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     //make request to the server and send the name
@@ -59,6 +58,8 @@ submit.onclick=function(){
   };
   
   //make the request
+  var nameInput=document.getElementById('name');
+  var name=nameInput.value;
   request.open('GET','http://keerthirajraja.imad.hasura-app.io/submit-name?name='+ name,true);
   request.send(null);
     
