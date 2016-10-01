a5fd09545d39577fe52046fee68ab57c4dde53bd
Keerthi Raj R <keerthichathri@gmail.com>
2016-10-01 23:23:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -83,6 +83,7 @@ app.get('/', function (req, res) {
 var counter=0;
 app.get('/counter', function(req,res){
     counter=counter+1;
+    console.log("counter",counter)
     res.send(counter.toString());
 });
 
